186cc64109d5e40e39f55b5642e2055a8ee94539
Heiko Bernloehr <Heiko.Bernloehr@FreeIT.de>
2015-04-22 12:16:20 +0200

    Change initial /sys/auths postroute to false.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
-# Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012
+# Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2015
 # Heiko Bernloehr (FreeIT.de).
-# 
+#
 # This file is part of ECS.
-# 
+#
 # ECS is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of
 # the License, or (at your option) any later version.
-# 
+#
 # ECS is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public
 # License along with ECS. If not, see <http://www.gnu.org/licenses/>.
 
@@ -34,5 +34,5 @@ Community.create :name => "public",
 end
 Ressource.create :namespace => 'sys',
   :ressource => 'auths',
-  :postroute => true,
+  :postroute => false,
   :events => false if Ressource.find_by_namespace_and_ressource("sys","auths").nil?
